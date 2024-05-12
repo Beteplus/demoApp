aba727fb5a0c31256083aaf7c48ae52d2e3b79ba
bete <beteme390@gmail.com>
2024-05-12 19:54:39 +0300

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,28 @@
 // import express from express
-import express from 'express'
 
-// const express = require('express');
+
+const express = require('express');
 const app = express();
+
+//import mysql
+
+const mysql = require("mysql2")
+// Create a connection pool
+const dbConfig = {
+    connectionLimit: "10",
+    host: "localhost",
+    user: "evanDemo",
+    password: "evandemo",
+    database: "evandemo"
+  };
+  // Get a connection from the pool
+const Connection = mysql.createConnection(dbConfig);
+Connection.connect((err)=>{
+if (err) throw err;
+console.log("Db connected");
+
+})
+
 
 app.get('/', (req,res)=>{
 res.send('testing')
